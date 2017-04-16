829be6b4cec73a0592d15eaf91ae80d0579c7f59
Maurik <Ferry.van.Maurik@nike.com>
2017-04-16 16:42:05 +0200

    What's makes up an HTTP request

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=3402DC%20aldrinhof%205%20ijsselstein',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    //console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
 });
