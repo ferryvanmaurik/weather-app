d2b66c31e12fa4b4cf5ba9181e473ddc33825e1d
Maurik <Ferry.van.Maurik@nike.com>
2017-04-26 10:17:37 +0200

    Callback errors

--- app.js
+++ app.js
@@ -21,8 +21,15 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    //console.log(JSON.stringify(body, undefined, 2));
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+    if (error) {
+        console.log('Unable to connect with Google API')
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find the address')
+    } else if (body.status === 'OK') {
+        //console.log(JSON.stringify(body, undefined, 2));
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+        console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+    }
+
 });
